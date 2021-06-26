4d3c9a0cf5bb03085ca9ac8729baf6a4d59030d9
mhaisane100 <mhaisane100@gmail.com>
2021-06-26 20:07:11 +0530

    test file

---
+++ ArithmaticDemo.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class ArithmaticDemo {
+	public static void main (String args[]) {
+		int divisor = 4;
+		int result = 2 ;
+		int rem = 3 ;
+		System.out.println("Result : " + result);
+		System.out.println("Remindar : " + rem);
+		int num = divisor * result + rem;
+ 		System.out.println("Number : " + num);
+	}
+}
